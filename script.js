851d5c9a3b0a056639631e9c596866ed99914c3d
SunnyColors <63605297+Karlito-blob@users.noreply.github.com>
2024-01-26 11:45:36 +0100

    Route Done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://whatyearisit-backend-pied-nu.vercel.app/year')
  .then(response => response.json())
  .then(data => {
-    document.querySelector('#year').textContent = data.now
+    document.querySelector('#year').textContent = data.year
  });
